a77b95dd3402995f1233788791bca45cf0a3aac1
noorqidam <noorqidam@gmail.com>
2019-09-01 11:12:21 +0700

    Tugas 2 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = 10;
+var string = "string";
+var boolean = true;
+var null1 = null;
+var array = [10,"string",false,null]
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(null1)
+console.log(array)
